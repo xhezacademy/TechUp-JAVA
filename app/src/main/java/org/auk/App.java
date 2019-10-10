e195b0fe16b806d7ecb08c39a368eeac9b9fa15f
Ylli Abazi <ylli.abazi019@gmail.com>
2019-10-10 23:24:57 +0200

    Ylli's solution of A1

--- app/src/main/java/org/auk/App.java
+++ app/src/main/java/org/auk/App.java
@@ -3,9 +3,16 @@ package org.auk;
 import org.auk.data.StudentRepository;
 import org.auk.models.Student;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 /**
  * TechUp JAVA - SAMS
@@ -13,6 +20,10 @@ import java.util.Locale;
 public class App 
 {
     private static int X_TIMES = 65;
+
+    private final static String DB_FILE_NAME = "database.txt";
+    private final static String dbFileName = System.getProperty("user.dir") + File.separator + "db" + File.separator + DB_FILE_NAME;
+
 
     private static StudentRepository repository;
 
@@ -26,6 +37,7 @@ public class App
         printTableBanner();
         printTableColumns();
         printTableBody();
+        sendToFile();
     }
 
     private static void printTableBody() {
@@ -70,6 +82,55 @@ public class App
                 "|                       Hello JAVA Gurus!                       |\n" +
                 "=".repeat(X_TIMES);
         print(banner);
+    }
+
+    private static void sendToFile() {
+        Scanner reader = new Scanner(System.in);
+
+        String[] columns = new String[]{"Name", "Surname", "Birthday", "Phone"};
+        StringBuilder sb = new StringBuilder();
+        boolean check = true;
+        String result = "";
+
+        while (check) {
+            for(int i = 0; i < 4; i++) {
+                System.out.println("Enter " + columns[i] + ": ");
+                sb.append(reader.nextLine() + ", ");
+            }
+
+            result = (checkID() + 1) + ", " + sb;
+
+            try {
+                Files.write(Path.of(dbFileName), (result + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Enter another record or exit? 'enter'/'exit'");
+            String secondInput = reader.nextLine();
+            if (secondInput.equals("exit")){
+                check = false;
+            }else if(secondInput.equals("enter")){
+                result = "";
+                check = true;
+            }
+        }
+    }
+
+    private static int checkID(){
+        int lastID = -1;
+        try {
+            List<String> lines = Files.readAllLines(Path.of(dbFileName));
+
+            for (var line : lines){
+                String[] values = line.split("\\s*,\\s*");
+                lastID = Integer.parseInt(values[0]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lastID;
     }
 
     /**
